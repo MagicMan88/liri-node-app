458193397be6075a45f37dc15981dc8055bac165
Stephen Green <stephengreen1028@gmail.com>
2020-01-03 20:07:46 -0700

    added spotify key to keys.js

---
+++ keys.js
@@ -0,0 +1,6 @@
+console.log('this is loaded');
+// spotify key
+exports.spotify = {
+    id: process.env.b0a6a0dd8d9b4c87b3e78259c926d852,
+    secret: process.env.d5e9af7c0fce4238873be99cff5e24d1
+};
