2e2ebbbb3b2ae8c4acb097f1bfe3ef0eb43daf75
Stephen Green <stephengreen1028@gmail.com>
2020-01-03 20:11:14 -0700

    created .env and added keys for spotify, created random.txt

--- keys.js
+++ keys.js
@@ -1,6 +1,7 @@
 console.log('this is loaded');
-// spotify key
+// spotify 
 exports.spotify = {
-    id: process.env.b0a6a0dd8d9b4c87b3e78259c926d852,
-    secret: process.env.d5e9af7c0fce4238873be99cff5e24d1
-};
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  };
+  
